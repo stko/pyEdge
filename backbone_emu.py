e123d239c895094ae421691b58059fafb739a306
Steffen Köhler <steffen@koehlers.de>
2022-08-08 14:36:31 +0200

    first bootstrapping steps

---
+++ backbone_emu.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+import pika
+import json
+
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='localhost', port= 5673,credentials=pika.PlainCredentials("myuser", "mypassword")))
+
+channel = connection.channel()
+
+channel.queue_declare(queue='bootstrap')
+
+
+def on_request(ch, method, props, body):
+    try:
+        data=json.loads(body)
+        duni_id=data["duni_id"]
+
+        print("duni_id",duni_id)
+        response = {
+            "repositories" : [
+                {
+            "docker_rep_url" : "localhost",
+            "docker_rep_user" : "user",
+            "docker_rep_password" : "password",
+                }
+            ],
+            "docker-compose":'''
+version: "3.9"
+services:
+  parent_company_rabbitmq:
+    container_name: "rabbitmq"
+    image: rabbitmq:management
+    ports:
+      # HTTP management UI
+      - '8080:15672'
+      # AMQP protocol port
+      - '5673:5672'
+    environment:
+    - RABBITMQ_DEFAULT_USER=myuser
+    - RABBITMQ_DEFAULT_PASS=mypassword
+  exchange:
+    container_name: "exchange"
+    # image: rabbitmq-management-with-federation
+    image: rabbitmq-management
+    build: ./exchange
+    #build:
+    #  context: .
+    #  dockerfile: ./Dockerfile
+    ports:
+      # HTTP management UI
+      - '8081:15672'
+      # AMQP protocol port
+      - '5672:5672'
+    environment:
+    - RABBITMQ_DEFAULT_USER=myuser
+    - RABBITMQ_DEFAULT_PASS=mypassword
+  demo:
+    container_name: demo
+    build: ./demo
+    environment:
+    - RABBITMQ_DEFAULT_USER=myuser
+    - RABBITMQ_DEFAULT_PASS=mypassword
+    - MODULE_NAME=demo
+    - MODULE_INDEX=1
+    - DUNI_ID=appolo13
+    depends_on:
+    - "exchange"
+        '''}
+
+        ch.basic_publish(exchange='',
+                        routing_key=props.reply_to,
+                        properties=pika.BasicProperties(correlation_id = \
+                                                            props.correlation_id),
+                        body=str(response))
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+    except Exception as ex:
+        print(str(ex))
+
+
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(queue='bootstrap', on_message_callback=on_request)
+
+print(" Awaiting RPC requests")
+channel.start_consuming()
+
