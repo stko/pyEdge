8f3a8e65a43d8be00e4df498ee9ad6023db2870f
Steffen Köhler <steffen@koehlers.de>
2022-08-17 15:21:49 +0200

    cleanup

--- rpc_client.py
+++ rpc_client.py
@@ -6,56 +6,11 @@ import uuid
 import json
 from pyedge import PyEdge
 
-
-class FibonacciRpcClient(object):
-
-    def __init__(self):
-
-        '''
-        self.connection = pika.BlockingConnection(
-            pika.ConnectionParameters(host=host,  credentials=pika.PlainCredentials(user, password)))
-
-        self.channel = self.connection.channel()
-
-        result = self.channel.queue_declare(queue='', exclusive=True)
-        self.callback_queue = result.method.queue
-
-        self.channel.basic_consume(
-            queue=self.callback_queue,
-            on_message_callback=self.on_response,
-            auto_ack=True)
-
-        self.response = None
-        self.corr_id = None
-
-    def on_response(self, ch, method, props, body):
-        if self.corr_id == props.correlation_id:
-            self.response = body
-
-    def call(self, n):
-        self.response = None
-        self.corr_id = str(uuid.uuid4())
-        self.channel.basic_publish(
-            exchange='',
-            routing_key='demo.1',
-            properties=pika.BasicProperties(
-                reply_to=self.callback_queue,
-                correlation_id=self.corr_id,
-            ),
-            body=json.dumps({"type":"egal","rpc":True,"data":str(n)})
-        )
-        self.connection.process_data_events(time_limit=None)
-        return int(self.response)
-
-
-fibonacci_rpc = FibonacciRpcClient()
-'''
-
 user =os.getenv("RABBITMQ_DEFAULT_USER") or "myuser"
 password= os.getenv("RABBITMQ_DEFAULT_PASS") or "mypassword"
 # helpful for debugging to set another exchange
-#host = sys.argv[1]
-host = 'localhost'
+host = sys.argv[1]
+#host = 'localhost'
 pe=PyEdge(
     "rpcclient.1",
     host=host,
@@ -64,5 +19,6 @@ pe=PyEdge(
 )
 print(" [x] Requesting fib(5)")
 response=pe.request("demo.1","egal","5")
+pe.close()
 print(" [.] Got %r" % response)
 
